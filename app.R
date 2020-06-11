f472b8506d73ff5ff9d59f4fc94edc0272fd6dbe
Deepak Karunakaran <deepakk@ieee.org>
2020-06-11 20:45:37 +1200

    COMMENTS ADDED
    - The R code was formatted.
    
    - Comments were added for the important modules.

--- app.R
+++ app.R
@@ -1,11 +1,4 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+ 
 library(shinydashboard)
 library(shiny)
 library(leaflet)
@@ -16,88 +9,93 @@ library(ggplot2)
 library(zoo)
 library(shinyWidgets)
 
-setwd("C:/Users/deepa/Desktop/police-qgis/files")
 
+### READ INPUT
+## This data was prepared using python.
 
-
-month_ <- "April"
-year_ <- "2018"
-
-
-### Data prepared using python is being added as time series
-crimes_data <- read.csv(file = "./data/police_filtered_data.csv")
-tweets_data <- read.csv(file = "./data/tweet_sentiment_ym.csv")
+crimes_data <- read.csv( file = "./data/police_filtered_data.csv" )
+tweets_data <- read.csv( file = "./data/tweet_sentiment_ym.csv" )
 
  
+### CRIME DATA
 
 ## converting the data to a timeseries
-crime_by_ym <- crimes_data %>% group_by(Year.Month)
-crime_by_ym <- aggregate(crimes_data $Victimisations, by=list(crimes_data$Year.Month), FUN=sum)
+crime_by_ym <- crimes_data %>% group_by( Year.Month )
+crime_by_ym <- aggregate( crimes_data $Victimisations, by=list(crimes_data$Year.Month ), FUN=sum )
 names(crime_by_ym)[1] <- "monthYear"
 names(crime_by_ym)[2] <- "crimeCount"
-crime_by_ym$monthYear <- as.yearmon(crime_by_ym$monthYear,format="%B  %Y")
-
-crime_count <- read.zoo(crime_by_ym)
-
-#twitter data
-
-names(tweets_data)[1] <- "monthYear"
-tweets_data$monthYear <- as.yearmon(tweets_data$monthYear,format="%Y-%m")
-
-tweets_data$all <- tweets_data$neg+tweets_data$pos+tweets_data$neu
-
-pos_tweets_by_ym <- select(tweets_data,c("monthYear","pos")) 
-neg_tweets_by_ym <- select(tweets_data,c("monthYear","neg")) 
-neu_tweets_by_ym <- select(tweets_data,c("monthYear","neu")) 
-all_tweets_by_ym <- select(tweets_data,c("monthYear","all")) 
+crime_by_ym$monthYear <- as.yearmon( crime_by_ym$monthYear,format="%B  %Y" )
+crime_count <- read.zoo( crime_by_ym )
 
 
 
-positive_tweets <- read.zoo(pos_tweets_by_ym)
-negative_tweets <- read.zoo(neg_tweets_by_ym)
-neutral_tweets <- read.zoo(neu_tweets_by_ym)
-all_tweets <- read.zoo(all_tweets_by_ym)
+### TWITTER DATA
+
+## handling datetime
+names( tweets_data )[1] <- "monthYear"
+tweets_data$monthYear <- as.yearmon( tweets_data$monthYear,format="%Y-%m" )
+tweets_data$all <- tweets_data$neg + tweets_data$pos+tweets_data$neu
+
+pos_tweets_by_ym <- select( tweets_data,c( "monthYear","pos" ) ) 
+neg_tweets_by_ym <- select( tweets_data,c( "monthYear","neg" ) ) 
+neu_tweets_by_ym <- select( tweets_data,c( "monthYear","neu" ) ) 
+all_tweets_by_ym <- select( tweets_data,c( "monthYear","all" ) )  
+
+## Tweets by Sentiment
+positive_tweets <- read.zoo( pos_tweets_by_ym )
+negative_tweets <- read.zoo( neg_tweets_by_ym )
+neutral_tweets <- read.zoo( neu_tweets_by_ym )
+all_tweets <- read.zoo( all_tweets_by_ym )
 
 
-### setting up the data for map
+
+### SETTING UP THE DATA FOR MAP
 
 pal <- colorBin("RdYlBu", domain = 0:39)
-nz_regions <- readOGR("./data/vector2/vector4.shp")
+## Load the vector files
+nz_regions <- readOGR("./data/vector2/vector.shp")
   
 
 
 
+
+### SETTING UP THE SHINY DASHBOARD
+
+## USER INTERFACE
+
 ui <- dashboardPage(
     skin = "blue",
-    dashboardHeader(title = "Dashboard"),
+    dashboardHeader( title = "Dashboard" ),
     dashboardSidebar(
-        sliderInput(inputId ="year_range",label = "Year:",
+      
+##Configuring the slider
+        sliderInput( inputId ="year_range", label = "Year:",
                     min = 2015,
                     max = 2019,
                     value = 1,
                     sep = "",
-                    step = 1),
+                    step = 1 ),
  
         sliderTextInput(
           inputId = "month_range", 
           label = "Month:", 
           grid = TRUE, 
           force_edges = TRUE,
-          choices = c("January","February","March","April","May","June",
-                      "July","August","September","October","November","December"
+          choices = c( "January", "February", "March", "April", "May", "June",
+                      "July", "August", "September", "October", "November", "December"
                       )
         )
-       
-         
     ),
+
+##Adding chloropleths and dygraphs with formatted headings
     dashboardBody(
-        fluidRow(column(width=12,box(width=12,leafletOutput(  outputId = "victimizations_Map"))),
+        fluidRow( column ( width=12, box( width=12, leafletOutput( outputId = "victimizations_Map") ) ),
          
         ),
         fluidRow(
-            dygraphOutput(outputId = "timeseriesplot")
+            dygraphOutput( outputId = "timeseriesplot" )
         ),
-        tags$head(tags$style(HTML(
+        tags$head( tags$style(HTML(
             '.myClass { 
         font-size: 20px;
         line-height: 50px;
@@ -107,86 +105,74 @@ ui <- dashboardPage(
         overflow: hidden;
         color: white;
       }
-    ')),
-        tags$script(HTML('
-      $(document).ready(function() {
-        $("header").find("nav").append(\'<span class="myClass"> NZ Police Choropleths </span>\');
-      })
-     '))
+    ') ),
+        tags$script( HTML('
+         $(document).ready(function() {
+         $("header").find("nav").append(\'<span class="myClass"> NZ Police Choropleths </span>\');
+      } )
+     ') )
     )
-        
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
- reactive({print(input$month_range)})  
-    
-  
-  data<-reactive({crimes_data[which(crimes_data$Year.Month == 
-                                      paste(input$month_range,input$year_range)),]})
-  
-  data_ordered <- reactive({
-    data()[order(match(data()$Territorial.Authority,nz_regions$NAME_2)),]$Victimisations
-    
+
+##SERVER LOGIC
+server <- function( input, output) {
+    reactive( { print( input$month_range ) } )
+    data<-reactive( { crimes_data[ which ( crimes_data$Year.Month == 
+                                      paste( input$month_range, input$year_range ) ), ] } )
+
+# The data needs to be ordered for redrawing the map, everytime a new date range is selected   
+  data_ordered <- reactive( {
+    data()[ order( match ( data()$Territorial.Authority,nz_regions$NAME_2 ) ), ]$Victimisations
+  } )
+  labels <-reactive( {
+    paste("<p>", nz_regions$NAME_2, "</P>"
+          ,"<p>", data_ordered(),
+    "</p>", sep = "")
   })
   
- 
-  labels <-reactive({
-    paste("<p>",nz_regions$NAME_2,"</P>"
-          ,"<p>",data_ordered(),
-    "</p>",sep = "")
-  })
   
-    output$timeseriesplot <- renderDygraph({
-        timeseriesData <- cbind(crime_count ,all_tweets,positive_tweets,
-                                neutral_tweets,negative_tweets)#(ldeaths,mdeaths, fdeaths)
-        dyRangeSelector(dygraph(timeseriesData, 
+# Adding the time series dygraph to the dashboard  
+  output$timeseriesplot <- renderDygraph( {
+        timeseriesData <- cbind( crime_count , all_tweets,positive_tweets,
+                                neutral_tweets, negative_tweets) 
+        dyRangeSelector( dygraph( timeseriesData, 
                                 main = "#crime and #tweets@nzpolice")%>%
-                          dyLegend(width = 600), 
-                        dateWindow = c("2014-07-01", "2020-03-01"))
+                                dyLegend(width = 600), 
+                                dateWindow = c( "2014-07-01", "2020-03-01" ) 
+                         )
     }
     )
 
-    output$victimizations_Map <- renderLeaflet(
-        
-
-      
+# Adding the map to the dashboard
+  output$victimizations_Map <- renderLeaflet(
        leaflet() %>% 
-         #addProviderTiles(providers$Esri.WorldStreetMap) %>%
-         addProviderTiles(providers$Stamen.TerrainBackground) %>%
-         setView(lat = -40.9006, lng  = 175.8860, zoom =4.5) %>%
-       addPolygons(data = nz_regions,
-                    weight =1,
-                   smoothFactor = 0.5,
-                   color = "white",
-                    fillOpacity = 0.8,
-                   fillColor = pal(log10(data_ordered())*10),
-                   highlightOptions = highlightOptions(
-                     weight=1,
-                     color = "green",
-                      
-                     fillOpacity = 0.7,
-                     bringToFront = TRUE
+         addProviderTiles( providers$Stamen.TerrainBackground ) %>%
+         setView( lat = -40.9006, lng  = 175.8860, zoom =4.5 ) %>%
+         addPolygons( data = nz_regions,
+                      weight =1,
+                      smoothFactor = 0.5,
+                      color = "white",
+                      fillOpacity = 0.8,
+                      fillColor = pal(log10(data_ordered())*10),
+                      highlightOptions = highlightOptions(
+                      weight=1,
+                      color = "green",
+                      fillOpacity = 0.7,
+                      bringToFront = TRUE
                    ),
                    label = lapply(labels(),HTML)) %>% 
-         addLegend(pal=pal,
-                   values = crimes_data[which(crimes_data$Year.Month == 
-                                                      paste(input$month_range,input$year_range)),]$Victimisations,
+         addLegend( pal=pal,
+                    values = crimes_data[ which( crimes_data$Year.Month == 
+                                                      paste( input$month_range,input$year_range ) ),]$Victimisations,
                    opacity = 0.7,
                    position="topright",
                    title = "Log scale")
-       
-        
-            # leaflet() %>%
-            # addProviderTiles(providers$Esri.WorldStreetMap) %>%
-            # setView(lat = -40.9006, lng  = 175.8860, zoom =4.5) %>%
-            # addPolygons(data= nz_regions,color = "#660000",weight=1)
-        
     )
- 
-     
 }
 
-# Run the application 
+
+
+# RUNNING THE APPLICATION
 shinyApp(ui = ui, server = server)
